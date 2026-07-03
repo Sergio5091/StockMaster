2ad5dca88a1cc389692803d43ab1a71258be1320
Manu Junior Babatoundé TOVIDEDJO <manu.junior09tovidedjo@gmail.com>
2026-07-03 20:50:53 +0200

    chore: remove unused Spring annotations from main application class
    - Remove @EnableJpaAuditing annotation
    - Remove @EnableScheduling annotation
    - Clean up unnecessary imports
    - Simplify application startup configuration

--- backend/stockmaster/src/main/java/com/backend/stockmaster/StockmasterApplication.java
+++ backend/stockmaster/src/main/java/com/backend/stockmaster/StockmasterApplication.java
@@ -2,12 +2,8 @@ package com.backend.stockmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableScheduling
 public class StockmasterApplication {
 
 	public static void main(String[] args) {
